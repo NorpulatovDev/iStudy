63f3304b78ce58ccba4c2633a523c383517ed1ac
ogabek_norpulatov <ogabeknorpulatov571@gmail.com>
2025-09-10 20:53:43 +0500

    swagger from http to https

--- src/main/java/com/ogabek/istudy/config/OpenApiConfig.java
+++ src/main/java/com/ogabek/istudy/config/OpenApiConfig.java
@@ -4,15 +4,28 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${app.base-url:http://localhost:8080}")
+    private String baseUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
+        // Create server configuration
+        Server server = new Server();
+        server.setUrl(baseUrl);
+        server.setDescription("iStudy API Server");
+
         return new OpenAPI()
+                .servers(List.of(server))  // This forces Swagger to use the correct URL
                 .info(new Info()
                         .title("iStudy API")
                         .version("1.0")
